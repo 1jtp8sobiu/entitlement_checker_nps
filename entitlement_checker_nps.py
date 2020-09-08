6454244a9d1a46d6c48a5abe20e77085d90a4653
1jtp8sobiu <3oxfoq0y@gmail.com>
2020-09-09 03:58:41 +0900

    Update script

--- entitlement_checker_nps.py
+++ entitlement_checker_nps.py
@@ -9,8 +9,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='PSN Entitlement Checker for NPS')
 parser.add_argument('input_file', help='input path of your internal_entitlements.json')
-parser.add_argument('--extractall', action='store_true', help='extract all items')
 parser.add_argument('--csv', action='store_true', help='output results as csv format(default=tsv format)')
+parser.add_argument('--debug', action='store_true', help='extract all items with extra info')
 args = parser.parse_args()
 
 psv = 'psv'
@@ -23,11 +23,81 @@ type_theme = 'THEME'
 type_avatar = 'AVATAR'
 type_game_dlc = 'GAME/DLC'
 
-def download_file(filename):
-    repo_url = 'https://raw.githubusercontent.com/1jtp8sobiu/entitlement_checker_nps/master/db_files/'
-    dist_dir = './db_files/'
+DB_DIR = './db_files/'
+
+def download_db():
+    base_url = 'https://raw.githubusercontent.com/1jtp8sobiu/entitlement_checker_nps/master/db_files/'
+    dist_dir = DB_DIR
     os.makedirs(dist_dir, exist_ok=True)
-    urllib.request.urlretrieve(repo_url+filename, dist_dir+filename)
+    
+    files = ['pkg.tsv', 'pkg_exception.tsv', 'zrif.tsv', 'rap.tsv', 'last_updated.txt']
+    for file in files:
+        urllib.request.urlretrieve(base_url+file, dist_dir+file)
+
+
+def load_db():
+    with open(DB_DIR+'pkg.tsv', newline='') as f:
+        pkg_db = f.read()
+    with open(DB_DIR+'pkg_exception.tsv', newline='') as f:
+        pkg_db += f.read()
+    with open(DB_DIR+'zrif.tsv', newline='') as f:
+        license_db = f.read()
+    with open(DB_DIR+'rap.tsv', newline='') as f:
+        license_db += f.read()
+        
+    return pkg_db, license_db
+
+
+def get_params(entitlement):
+    try:
+        pkg = entitlement['drm_def']['drmContents'][0]['contentUrl']
+        pkg_domain = pkg.split('/')[2]
+        pkg_filename = pkg.split('/')[-1]
+    except:
+        return None
+
+    if pkg_domain.split('.')[0] not in ['ares', 'zeus', 'hfs']:
+        return None
+
+    active_date = entitlement['active_date']
+    cid = entitlement['drm_def']['drmContents'][0]['contentId']
+    name = entitlement['drm_def']['drmContents'][0]['contentName']
+    size = entitlement['drm_def']['drmContents'][0]['contentSize']
+    drm_type = entitlement['drm_def']['drmContents'][0]['drmType']
+    platform_id = entitlement['drm_def']['drmContents'][0]['platformIds']
+    base_game = entitlement['drm_def']['drmContents'][0]['titleName']
+    product_id = entitlement['drm_def']['productId']
+
+    if cid == 'EP0001-NPEB01982_00-FC4GAMEPS3000001':
+        drm_type = 3
+
+    try:
+        package_sub_type = entitlement['game_meta']['package_sub_type']
+    except:
+        package_sub_type = ''
+
+    try:
+        psplus = entitlement['drm_def']['expiration']
+        psplus = 'Yes'
+    except:
+        psplus = ''
+
+    params = {'pkg': pkg,
+              'pkg_domain': pkg_domain,
+              'pkg_filename': pkg_filename,
+              'active_date': active_date,
+              'cid': cid,
+              'name': name,
+              'size': size,
+              'drm_type': drm_type,
+              'platform_id': platform_id,
+              'base_game': base_game,
+              'product_id': product_id,
+              'package_sub_type': package_sub_type,
+              'psplus': psplus}
+
+    return params
+
 
 def get_platform(platform_id, cid, package_sub_type):
     if platform_id == 2281701376: # (or platform_id == 4293918720 ?)
@@ -62,16 +132,37 @@ def get_platform(platform_id, cid, package_sub_type):
         pkg_type = type_avatar
 
     #Special CIDs
-    if cid == 'EP9000-PCSF00178_00-SOULENTITLEMENT1' or cid == 'UP1082-NPUB90491_00-SPLITSECONDDEM21':
+    if cid == 'EP9000-PCSF00178_00-SOULENTITLEMENT1':
         platform = ps3
-    elif cid == 'JP9002-NPJW90014_00-0000000000000003': # platform_id == 2014314496
+    if cid == 'UP1082-NPUB90491_00-SPLITSECONDDEM21':
+        platform = ps3
+    if cid == 'JP9002-NPJW90014_00-0000000000000003': # platform_id == 2014314496
         platform = psp
         pkg_type = type_theme
-    elif cid[20:35] == 'KAGURAPLUSTHEME':
+    if cid[20:35] == 'KAGURAPLUSTHEME':
         platform = ps3
         pkg_type = type_theme
 
     return platform, pkg_type
+
+
+def is_pkg_missing(pkg_filename, pkg_db):
+    if pkg_filename in pkg_db:
+        return False
+    else:
+        return True
+
+
+def is_license_missing(platform, drm_type, cid, license_db):
+    if platform == psp:
+        return False
+    if drm_type == 3 or drm_type == 13:
+        return False
+    if cid in license_db:
+        return False
+    else:
+        return True
+
 
 class PlatformData:
     def __init__(self, platform):
@@ -97,22 +188,32 @@ class PlatformData:
 
         self.output_json_name = platform + '_submission.json'
 
-    def add_new_item(self, pkg, active_date, cid, name, size, drm_type, platform_id, base_game, product_id, package_sub_type, missing_pkg, missing_license, psplus):
-            if self.item_count == 0:
-                if args.extractall:
-                    csv_header = ['Item No.', 'Content Name', 'Platform', '*Missing PKG', self.csv_header_license, 'PS Plus', 'Content ID', 'Size', 'Date', 'Base Game', 'titleid', 'platform_id', 'product_id', 'drm_type', 'package_sub_type', 'pkg']
-                else:
-                    csv_header = ['Item No.', 'Content Name', 'Platform', '*Missing PKG', self.csv_header_license, 'PS Plus', 'Content ID', 'Size', 'Date', 'Base Game']
-                self.results.append(csv_header)
-
-            if args.extractall:
-                newrow = [self.item_count+1, name, self.plat_name, missing_pkg, missing_license, psplus, cid, size, active_date[0:10], base_game, cid[7:16], platform_id, product_id, drm_type, package_sub_type, pkg]
+    def add_new_item(self, p):
+        if self.item_count == 0:
+            if args.debug:
+                csv_header = ['Item No.', 'Content Name', 'Platform', '*Missing PKG', self.csv_header_license,
+                              'PS Plus', 'Content ID', 'Size', 'Date', 'Base Game',
+                              'titleid', 'platform_id', 'product_id', 'drm_type', 'package_sub_type', 'pkg']
             else:
-                newrow = [self.item_count+1, name, self.plat_name, missing_pkg, missing_license, psplus, cid, size, active_date[0:10], base_game]
-            self.results.append(newrow)
-            new_item = {'platform': self.plat_name, 'name': name, 'pkg': pkg, 'id': cid, 'size': size, 'baseGame': base_game, 'productID': product_id, 'url': ''}
-            self.submission_json['items'].append(new_item)
-            self.item_count += 1
+                csv_header = ['Item No.', 'Content Name', 'Platform', '*Missing PKG', self.csv_header_license,
+                              'PS Plus', 'Content ID', 'Size', 'Date', 'Base Game']
+            self.results.append(csv_header)
+
+        if args.debug:
+            newrow = [self.item_count+1, p['name'], self.plat_name, p['missing_pkg'], p['missing_license'],
+                      p['psplus'], p['cid'], p['size'], p['active_date'][0:10], p['base_game'],
+                      p['cid'][7:16], p['platform_id'], p['product_id'], p['drm_type'], p['package_sub_type'], p['pkg']]
+        else:
+            newrow = [self.item_count+1, p['name'], self.plat_name, p['missing_pkg'], p['missing_license'], 
+                      p['psplus'], p['cid'], p['size'], p['active_date'][0:10], p['base_game']]
+                      
+        self.results.append(newrow)
+        
+        new_item = {'platform': self.plat_name, 'name': p['name'], 'pkg': p['pkg'], 'id': p['cid'],
+                    'size': p['size'], 'baseGame': p['base_game'], 'productID': p['product_id'], 'url': ''}
+        
+        self.submission_json['items'].append(new_item)
+        self.item_count += 1
 
     def output(self):
         dist_dir = './output/'
@@ -131,131 +232,92 @@ class PlatformData:
                 json.dump(self.submission_json, f, ensure_ascii=False, indent=4)
             print(f'{dist_dir}{self.output_json_name:.<24s}done')
 
+
 def main():
     try:
         with open(args.input_file, 'rb') as f:
             entitlement_json = json.loads(f.read())
-        item_count_all = len(entitlement_json['entitlements'])
+        data_length = len(entitlement_json['entitlements'])
     except:
-         print('Error!...Invalid json file.\n')
+         print('Error!...Invalid json file.', '\n')
          input('Press Enter to exit.')
-         sys.exit()
+         sys.exit(1)
 
     dpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     os.chdir(dpath)
 
-    print('Downloading db files...')
+    print('Downloading database files', end='', flush=True)
     try:
-        download_file('pkg.tsv')
-        download_file('pkg_exception.tsv')
-        download_file('zrif.tsv')
-        download_file('rap.tsv')
-        download_file('last_updated.txt')
+        download_db()
     except:
         pass
+    print('.......done')
 
     try:
-        with open('./db_files/pkg.tsv', newline='') as f:
-            pkg_list = f.read()
-        with open('./db_files/pkg_exception.tsv', newline='') as f:
-            pkg_exception_list = f.read()
-        with open('./db_files/zrif.tsv', newline='') as f:
-            license_list = f.read()
-        with open('./db_files/rap.tsv', newline='') as f:
-            license_list += f.read()
+        pkg_db, license_db = load_db()
     except:
-         print('Error!...Couldn\'t open files.\n')
+         print('Error!...Could not open the database files.', '\n')
          input('Press Enter to exit.')
-         sys.exit()
+         sys.exit(1)
 
+    print(f'Checking {data_length:>4d} items', end='', flush=True)
     psv_data = PlatformData(psv)
     ps3_data = PlatformData(ps3)
     psp_data = PlatformData(psp)
-
-    print(f'Checking {item_count_all} items...')
     for entitlement in reversed(entitlement_json['entitlements']):
-        try:
-            pkg = entitlement['drm_def']['drmContents'][0]['contentUrl']
-            pkg_domain = pkg.split('/')[2]
-            pkg_filename = pkg.split('/')[-1]
-        except:
-            pkg = False
-            pkg_domain = False
-            pkg_filename = False
-
-        if not (pkg_domain == 'ares.dl.playstation.net' or pkg_domain == 'zeus.dl.playstation.net' or pkg_domain == 'hfs.dl.playstation.net'):
+        p = get_params(entitlement)
+        if not p:
             continue
 
-        active_date = entitlement['active_date']
-        cid = entitlement['drm_def']['drmContents'][0]['contentId']
-        name = entitlement['drm_def']['drmContents'][0]['contentName']
-        size = entitlement['drm_def']['drmContents'][0]['contentSize']
-        drm_type = entitlement['drm_def']['drmContents'][0]['drmType']
-        platform_id = entitlement['drm_def']['drmContents'][0]['platformIds']
-        base_game = entitlement['drm_def']['drmContents'][0]['titleName']
-        product_id = entitlement['drm_def']['productId']
+        p['platform'], p['pkg_type'] = get_platform(p['platform_id'], p['cid'], p['package_sub_type'])
 
-        if cid == 'EP0001-NPEB01982_00-FC4GAMEPS3000001':
-            drm_type = 3
-
-        try:
-            package_sub_type = entitlement['game_meta']['package_sub_type']
-        except:
-            package_sub_type = False
-
-        platform = get_platform(platform_id, cid, package_sub_type)[0]
-        pkg_type = get_platform(platform_id, cid, package_sub_type)[1]
-
-        try:
-            psplus = entitlement['drm_def']['expiration']
-            psplus = 'Yes'
-        except:
-            psplus = ''
-
-        if pkg_filename not in pkg_list and pkg_filename not in pkg_exception_list:
-            missing_pkg = 'Yes'
+        if is_pkg_missing(p['pkg_filename'], pkg_db):
+            p['missing_pkg'] = 'Yes'
         else:
-            missing_pkg = ''
-        if (platform == psv or platform == ps3) and not (drm_type == 3 or drm_type == 13) and cid not in license_list:
-            missing_license = 'Yes'
-        else:
-            missing_license = ''
-            
-        if args.extractall:
-            pass
-        else:
-            if missing_pkg == '' and missing_license == '':
-                continue
+            p['missing_pkg'] = ''
 
-        item_data = (pkg, active_date, cid, name, size, drm_type, platform_id, base_game, product_id, package_sub_type, missing_pkg, missing_license, psplus)
-        if platform == psv:
-            psv_data.add_new_item(*item_data)
-        elif platform == ps3:
-            ps3_data.add_new_item(*item_data)
-        elif platform == psp:
-            psp_data.add_new_item(*item_data)
+        if is_license_missing(p['platform'], p['drm_type'], p['cid'], license_db):
+            p['missing_license'] = 'Yes'
+        else:
+            p['missing_license'] = ''
 
-    print('Done.\n')
+        if p['missing_pkg'] or p['missing_license'] or args.debug:
+            if p['platform'] == psv:
+                psv_data.add_new_item(p)
+            elif p['platform'] == ps3:
+                ps3_data.add_new_item(p)
+            elif p['platform'] == psp:
+                psp_data.add_new_item(p)
+
+        if args.debug:
+            print(p['pkg'])
+
+    print('..............done', '\n')
 
     if psv_data.item_count == 0 and ps3_data.item_count == 0 and psp_data.item_count == 0:
-        print('All items are already in the DB.\n')
+        print('All items are already in the database.', '\n')
     else:
         try:
             psv_data.output()
             ps3_data.output()
             psp_data.output()
         except:
-            print('Error!...Couldn\'t create result files\n')
+            print('Error!...Could not create the result files.', '\n')
             input('Press Enter to exit.')
-            sys.exit()
+            sys.exit(1)
 
-        print()
-        print('Please check "xxx_results.tsv" and create missing NoNpDrm/RAP licenses.')
-        print('Then submit "xxx_submission.json" along with the created licenses (zipped file) to the NPS DB.')
-        print('For more details, see the contribution FAQ. https://nopaystation.com/faq \n')
+        if not args.debug:
+            found = psv_data.item_count + ps3_data.item_count + psp_data.item_count
+            print(f'Found {found} items in total!', '\n')
+            print('* Please check "xxx_results.tsv" and create missing NoNpDrm/RAP licenses.')
+            print('* Then submit "xxx_submission.json" along with the created licenses (zipped file) on')
+            print('* the NPS Contribution page. [ https://nopaystation.com/contribute/batch ]')
+            print('*')
+            print('* For more details, see the contribution FAQ. [ https://nopaystation.com/faq ] \n')
 
     input('Press Enter to exit.')
-    sys.exit()
+    sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
